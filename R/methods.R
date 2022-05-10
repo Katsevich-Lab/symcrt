cd948a7695c2325c1e6867519055f941bf24983f
Ziang Niu <ziangniu6@gmail.com>
2022-05-10 16:40:54 -0400

    add argument to MaxwayCRT

--- R/methods.R
+++ R/methods.R
@@ -249,19 +249,48 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
   Z <- data$Z
   n <- nrow(Z)
   d <- ncol(Z)
+  if(test_hyperparams$unlabel_prop == 0){
+    X_unlabel <- data$X_unlabel
+    Z_unlabel <- data$Z_unlabel 
+    no_unlabel <- nrow(Z_unlabel)
+    index_unlabel <- sample(1:n, no_unlabel)
+    
+    if(is.null(X_unlabel)){
+      stop("Unlabel data should be provided!")
+    }
+    
+    # extract the proportion of the number of holdout data and sample 1:n
+    no_holdout <- round(n*test_hyperparams$holdout_prop)
+    index_holdout <- sample(1:n, no_holdout)
+    
+    # extract the index that used for testing
+    index_test <- setdiff(1:n, index_holdout)
+  }else{
+    # extract the proportion of the number of unlabelled data and sample 1:n
+    no_unlabel <- round(n*test_hyperparams$unlabel_prop)
+    index_unlabel <- sample(1:n, no_unlabel)
+    
+    # assign the unlabel data
+    X_unlabel <- X[index_unlabel]
+    Z_unlabel <- Z[index_unlabel, ]
+    
+    # find the rest data index
+    index_rest <- setdiff(1:n, index_unlabel)
+    
+    # extract the proportion of the number of holdout data and sample 1:n
+    no_holdout <- round(n*test_hyperparams$holdout_prop)
+    index_holdout <- sample(index_rest, no_holdout)
+    
+    # extract the index that used for testing
+    index_test <- setdiff(index_rest, index_holdout)
+  }
+
+  
+  
 
 
-  # extract the proportion of the number of unlabelled data and sample 1:n
-  no_unlabel <- round(n*test_hyperparams$unlabel_prop)
-  index_unlabel <- sample(1:n, no_unlabel)
-  index_rest <- setdiff(1:n, index_unlabel)
 
-  # extract the proportion of the number of holdout data and sample 1:n
-  no_holdout <- round(n*test_hyperparams$holdout_prop)
-  index_holdout <- sample(index_rest, no_holdout)
 
-  # extract the index that used for testing
-  index_test <- setdiff(index_rest, index_holdout)
 
   # fit conditional mean of X given Z with unlablled data
   if (X_on_Z_reg$mean_method_type == "oracle") {
@@ -269,8 +298,8 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
     E_X_given_Z <- E_X_given_Z_full[index_unlabel]
     if (is.null(E_X_given_Z)) stop("Must specify cond_mean if mean_method_type is oracle")
   } else {
-    fit_X_given_Z <- fit_conditional_mean(X[index_unlabel],
-                                          Z[index_unlabel,],
+    fit_X_given_Z <- fit_conditional_mean(X_unlabel,
+                                          Z_unlabel,
                                           X_on_Z_reg)
     E_X_given_Z <- fit_X_given_Z$conditional_mean
     coef_X_given_Z <- fit_X_given_Z$coef_vec
@@ -307,13 +336,13 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
   }
 
   # concatenate predictor and active Z together
-  predictor_Y_Z_unlabel <- cbind(1, Z[index_unlabel, ])%*%coef_Y_given_Z
+  predictor_Y_Z_unlabel <- cbind(1, Z_unlabel)%*%coef_Y_given_Z
   predictor_Y_Z_test <- cbind(1, Z[index_test, ])%*%coef_Y_given_Z
   if(length(act_set_Y_given_Z) == 0){
-    g_Z_unlabel <- stats::rnorm(length(index_unlabel))
+    g_Z_unlabel <- stats::rnorm(no_unlabel)
     g_Z_test <- stats::rnorm(length(index_test))
   }else{
-    g_Z_unlabel <- orthogonalize(predictor_Y_Z_unlabel, Z[index_unlabel, act_set_Y_given_Z])
+    g_Z_unlabel <- orthogonalize(predictor_Y_Z_unlabel, Z_unlabel[, act_set_Y_given_Z])
     g_Z_test <- orthogonalize(predictor_Y_Z_test, Z[index_test, act_set_Y_given_Z])
   }
 
@@ -341,7 +370,7 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
            #   h_Z_unlabel <- rnorm(length(index_unlabel))
            #   h_Z_test <- rnorm(length(index_test))
            # }else{
-           #   h_Z_unlabel <- cbind(1, Z[index_unlabel, ])%*%coef_X_given_Z
+           #   h_Z_unlabel <- cbind(1, Z_unlabel)%*%coef_X_given_Z
            #   h_Z_test <- cbind(1, Z[index_test, ])%*%coef_X_given_Z
            # }
            #
@@ -352,8 +381,8 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
            # residual_on_g_h <- list(mean_method_type = "MLE",
            #                         mean_method_hyperparams = list(family = "binomial"),
            #                         var_method_type = "homoskedastic")
-           # fit_X_on_g_h <- fit_conditional_mean(X[index_unlabel], g_h_Z_unlabel, residual_on_g_h)
-           fit_X_on_g_h <- stats::glm(X[index_unlabel] ~ g_Z_unlabel, offset = logit(E_X_given_Z), family = "binomial")
+           # fit_X_on_g_h <- fit_conditional_mean(X_unlabel, g_h_Z_unlabel, residual_on_g_h)
+           fit_X_on_g_h <- stats::glm(X_unlabel ~ g_Z_unlabel, offset = logit(E_X_given_Z), family = "binomial")
            # resample matrix from the specified distribution
            resample_mean <- glogit(logit(E_X_given_Z_test) + as.vector(cbind(1, g_Z_test)%*%(fit_X_on_g_h$coefficients)))
            resample_var <- resample_mean*(1-resample_mean)
@@ -382,7 +411,7 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
          gaussian = {
 
            # compute the residual and fit conditional mean on label data
-           r_unlabel <- X[index_unlabel] - cbind(1, Z[index_unlabel, ])%*%coef_X_given_Z
+           r_unlabel <- X_unlabel - cbind(1, Z_unlabel)%*%coef_X_given_Z
            r_label <- X[index_test] - cbind(1, Z[index_test, ])%*%coef_X_given_Z
            E_X_given_Z_test <- cbind(1, Z[index_test, ])%*%coef_X_given_Z
 
@@ -429,201 +458,3 @@ MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
     value = c(p_value)
   )
 }
-
-#' #' Maxway CRT proposed in Li & Liu 2022.
-#' #'
-#' #' @param data A named list with fields X, Y, Z.
-#' #' @param X_on_Z_reg The regression method to apply for X|Z.
-#' #' @param Y_on_Z_reg The regression method to apply for Y|Z.
-#' #' @param test_hyperparams Additional test hyperparameters
-#' 
-#' #' @return A data frame with columns "parameter," "target," "value" with p-value.
-#' #' @export
-#' MaxwayCRT <- function(data, X_on_Z_reg, Y_on_Z_reg, test_hyperparams) {
-#'   # TODO: Write a test for this function as follows: Give it a Gaussian resampling
-#'   # distribution, and then compare the output to Molei's implementation
-#'   test_hyperparams <- set_default_test_hyperparams("dCRT", test_hyperparams)
-#'   # extract X, Y, Z from first input argument
-#'   X <- data$X
-#'   Y <- data$Y
-#'   Z <- data$Z
-#'   n <- nrow(Z)
-#'   d <- ncol(Z)
-#'   
-#'   # extract the proportion of the number of unlabelled data and sample 1:n
-#'   no_unlabel <- round(n*test_hyperparams$unlabel_prop)
-#'   index_unlabel <- sample(1:n, no_unlabel)
-#'   index_rest <- setdiff(1:n, index_unlabel)
-#'   
-#'   # extract the proportion of the number of holdout data and sample 1:n
-#'   no_holdout <- round(n*test_hyperparams$holdout_prop)
-#'   index_holdout <- sample(index_rest, no_holdout)
-#'   
-#'   # extract the index that used for testing
-#'   index_test <- setdiff(index_rest, index_holdout)
-#'   print(d)
-#'   # fit conditional mean of X given Z with unlablled data
-#'   if (X_on_Z_reg$mean_method_type == "oracle") {
-#'     E_X_given_Z_full <- data$E_X_given_Z_oracle
-#'     E_X_given_Z <- E_X_given_Z_full[index_unlabel]
-#'     if (is.null(E_X_given_Z)) stop("Must specify cond_mean if mean_method_type is oracle")
-#'   } else {
-#'     fit_X_given_Z <- fit_conditional_mean(X[index_unlabel], 
-#'                                           Z[index_unlabel,], 
-#'                                           X_on_Z_reg)
-#'     E_X_given_Z <- fit_X_given_Z$conditional_mean
-#'     coef_X_given_Z <- fit_X_given_Z$coef_vec
-#'   }
-#'   
-#'   # # fit conditional variance of X given Z
-#'   # if (X_on_Z_reg$var_method_type == "oracle") {
-#'   #   Var_X_given_Z <- data$Var_X_given_Z_oracle
-#'   #   if (is.null(Var_X_given_Z)) stop("Must specify cond_var if var_method_type is oracle")
-#'   # } else {
-#'   #   Var_X_given_Z <- fit_conditional_variance(X, Z, E_X_given_Z, X_on_Z_reg$var_method_type)
-#'   # }
-#'   
-#'   # fit conditional mean of Y given Z; construct g(Z)
-#'   if(no_holdout == 0){
-#'     fit_Y_given_Z <- fit_conditional_mean(Y[index_test], 
-#'                                           Z[index_test, ],
-#'                                           Y_on_Z_reg)
-#'   }else{
-#'     fit_Y_given_Z <- fit_conditional_mean(Y[index_holdout], 
-#'                                           Z[index_holdout, ],
-#'                                           Y_on_Z_reg)
-#'   }
-#'   E_Y_given_Z <- fit_Y_given_Z$conditional_mean
-#'   coef_Y_given_Z <- as.vector(fit_Y_given_Z$coef_vec)
-#'   
-#'   
-#'   # extract the active set part in g(Z)
-#'   if(test_hyperparams$g_Z_support == "support of beta_hat"){
-#'     act_set_Y_given_Z <- which(coef_Y_given_Z[-1] != 0)
-#'     print(length(act_set_Y_given_Z))
-#'     if(is.null(act_set_Y_given_Z)){
-#'       print("Null")
-#'     }
-#'   }else if(test_hyperparams$g_Z_support == "default"){
-#'     no_act_set_Y_given_Z <- as.integer(min(2 * log(d), d - 1, (no_unlabel)^(1/3)))
-#'     act_set_Y_given_Z <- order(abs(coef_Y_given_Z), decreasing=TRUE)[1:no_act_set_Y_given_Z]
-#'   }
-#'   
-#'   # concatenate predictor and active Z together
-#'   predictor_Y_Z_unlabel <- cbind(1, Z[index_unlabel, ])%*%coef_Y_given_Z
-#'   predictor_Y_Z_test <- cbind(1, Z[index_test, ])%*%coef_Y_given_Z
-#'   if(length(act_set_Y_given_Z) == 0){
-#'     g_Z_unlabel <- Y[index_unlabel]
-#'     g_Z_test <- Y[index_test]
-#'   }else{
-#'     g_Z_unlabel <- orthogonalize(predictor_Y_Z_unlabel, Z[index_unlabel, act_set_Y_given_Z])
-#'     g_Z_test <- orthogonalize(predictor_Y_Z_test, Z[index_test, act_set_Y_given_Z])
-#'   }
-#'   
-#'   # fit the conditional expectation of Y|Z on label data
-#'   Y_on_Z_reg_hyperparams <- Y_on_Z_reg$mean_method_hyperparams
-#'   switch(Y_on_Z_reg_hyperparams$family,
-#'          binomial = {
-#'            E_Y_given_Z_test <- 1/exp(-cbind(1, Z[index_test, ])%*%coef_Y_given_Z)
-#'          },
-#'          gaussian = {
-#'            E_Y_given_Z_test <- cbind(1, Z[index_test, ])%*%coef_Y_given_Z
-#'          },
-#'          {
-#'            stop("The rsampling distribution of X given Z is invalid!")
-#'          }
-#'   )
-#'   
-#'   # learning the resampling distribution with unlabelled data
-#'   switch(test_hyperparams$resample_family,
-#'          binomial = {
-#'            
-#'            # construct h(Z)=Z%*%gamma_hat and fit conditional mean on label data
-#'            h_Z_unlabel <- cbind(1, Z[index_unlabel, ])%*%coef_X_given_Z
-#'            h_Z_test <- cbind(1, Z[index_test, ])%*%coef_X_given_Z
-#'            g_h_Z_unlabel <- cbind(g_Z_unlabel, h_Z_unlabel)
-#'            E_X_given_Z_test <- 1/exp(-cbind(1, Z[index_test, ])%*%coef_X_given_Z)
-#'            
-#'            # fit X on g and h
-#'            residual_on_g_h <- list(mean_method_type = "MLE",
-#'                                    mean_method_hyperparams = list(family = "binomial"),
-#'                                    var_method_type = "homoskedastic")
-#'            fit_X_on_g_h <- fit_conditional_mean(X[index_unlabel], g_h_Z_unlabel, residual_on_g_h)
-#'            Var_X_on_g_h <- fit_X_on_g_h$conditional_mean*(1 - fit_X_on_g_h$conditional_mean)
-#'            
-#'            # resample matrix from the specified distribution
-#'            g_h_Z_test <- cbind(g_Z_test, h_Z_test)
-#'            resample_mean <- 1/(1+exp(-cbind(1, g_h_Z_test)%*%(fit_X_on_g_h$coef_vec)))
-#'            resample_var <- Var_X_on_g_h
-#'            resample_matrix <- resample_dCRT(conditional_mean = resample_mean,
-#'                                             conditional_variance = resample_var,
-#'                                             no_resample = test_hyperparams$no_resample,
-#'                                             resample_dist = test_hyperparams$resample_family)
-#'            resample_X_residuals <- resample_matrix - resample_mean
-#'          },
-#'          gaussian = {
-#'            
-#'            # compute the residual and fit conditional mean on label data
-#'            r_unlabel <- X[index_unlabel] - cbind(1, Z[index_unlabel, ])%*%coef_X_given_Z
-#'            r_label <- X[index_test] - cbind(1, Z[index_test, ])%*%coef_X_given_Z
-#'            E_X_given_Z_test <- cbind(1, Z[index_test, ])%*%coef_X_given_Z
-#'            
-#'            # fit the residual and g(Z) with lasso
-#'            residual_on_gZ <- list(mean_method_type = "MLE",
-#'                                   mean_method_hyperparams = list(family = "gaussian"),
-#'                                   var_method_type = "homoskedastic")
-#'            fit_residual_g_Z <- fit_conditional_mean(r_unlabel, g_Z_unlabel, residual_on_gZ)
-#'            Var_residual_g_Z <- fit_conditional_variance(r_unlabel, 
-#'                                                         g_Z_unlabel, 
-#'                                                         fit_residual_g_Z$conditional_mean, 
-#'                                                         X_on_Z_reg$var_method_type)
-#'            
-#'            # resample matrix from the specified distribution
-#'            resample_mean <- cbind(1, g_Z_test)%*%(fit_residual_g_Z$coef_vec)
-#'            resample_var <- Var_residual_g_Z
-#'            resample_matrix <- resample_dCRT(conditional_mean = resample_mean,
-#'                                             conditional_variance = resample_var,
-#'                                             no_resample = test_hyperparams$no_resample,
-#'                                             resample_dist = test_hyperparams$resample_family)
-#'            resample_X_residuals <- resample_matrix
-#'          },
-#'          {
-#'            stop("The rsampling distribution of X given Z is invalid!")
-#'          }
-#'   )
-#'   
-#'   
-#'   
-#'   # define the test statistic
-#'   X_residuals <- c(X[index_test] - E_X_given_Z_test)
-#'   Y_residuals <- c(Y[index_test] - E_Y_given_Z_test)
-#'   test_statistic <- 1 / (sqrt(n)) * sum(X_residuals * Y_residuals)
-#'   
-#'   
-#'   # compute the residuals and variance vector for each resample
-#'   residual_star <- apply(resample_X_residuals * Y_residuals, 2, sum)
-#'   
-#'   # compute the resample test statistic and quantile
-#'   resample_test_statistic <- 1 / (sqrt(n)) * residual_star
-#'   no_exceed <- length(which(abs(resample_test_statistic) >= abs(test_statistic)))
-#'   
-#'   # compute the p-value (two sided)
-#'   p_value <- (no_exceed + 1) / (test_hyperparams$no_resample + 1)
-#'   
-#'   if(test_hyperparams$resample_family == "gaussian"){
-#'     X_residuals <- r_label - cbind(1, g_Z_test)%*%(fit_residual_g_Z$coef_vec)
-#'     X_residuals <- X_residuals/sd(X_residuals)
-#'     n_test <- length(X_residuals)
-#'     imp_obe <- abs(mean(X_residuals * Y_residuals))
-#'     emp_var <- mean(Y_residuals^2)
-#'     p_value <- 2 * pnorm(- sqrt(n_test) * imp_obe / sqrt(emp_var))
-#'   }
-#'   
-#'   # output the results
-#'   data.frame(
-#'     parameter = c("p_value"),
-#'     target = "conditional_independence",
-#'     value = c(p_value)
-#'   )
-#' }
-
--- tests/testthat/test-methods.R
+++ tests/testthat/test-methods.R
@@ -215,3 +215,96 @@ test_that("MaxwayCRT works for Gaussian", {
   )
 })
 
+
+
+test_that("MaxwayCRT works for Gaussian", {
+  
+  # test whether a specific example gives exactly the right answer
+  # (generated using dput)
+  source("function.R")
+  set.seed(1)
+  n <- 100
+  d <- 1000
+  s <- 5
+  rho <- 0.5
+  N <- n
+  gamma <- numeric(d)
+  gamma[1:s] <- 2 * rbinom(s, 1, 0.5) - 1
+  sig_Z <- katlabutils::generate_cov_ar1(rho, d)
+  Z <- katlabutils::fast_generate_mvn(numeric(d), sig_Z, N)
+  noise <- rnorm(2 * n)
+  E_X_given_Z_oracle <- Z %*% gamma |> as.vector()
+  Var_X_given_Z_oracle <- rep(1, n)
+  X <- E_X_given_Z_oracle + noise[1:n]
+  Y <- Z %*% gamma + noise[(n + 1):(2 * n)]
+  Z_unlabel <- matrix(rnorm(n = N * d), nrow = N, ncol = d)
+  E_X_given_Z_unlabel <- Z_unlabel %*% gamma |> as.vector()
+  X_unlabel <- E_X_given_Z_unlabel + rnorm(N)
+  data <- list(
+    Z = Z,
+    X = X,
+    Y = Y,
+    E_X_given_Z_oracle = E_X_given_Z_oracle,
+    Var_X_given_Z_oracle = Var_X_given_Z_oracle,
+    Z_unlabel = Z_unlabel,
+    X_unlabel = X_unlabel
+  )
+  X_on_Z_reg <- list(
+    mean_method_type = "LASSO",
+    mean_method_hyperparams = list(
+      s = "lambda.min",
+      family = "gaussian"
+    ),
+    var_method_type = "homoskedastic"
+  )
+  
+  Y_on_Z_reg <- list(
+    mean_method_type = "LASSO",
+    mean_method_hyperparams = list(
+      s = "lambda.min",
+      family = "gaussian"
+    ),
+    var_method_type = "homoskedastic"
+  )
+  
+  test_hyperparams <- list(resample_family = "gaussian", 
+                           no_resample = 5000,
+                           unlabel_prop = 0.5)
+  
+  
+  # test whether we get similar p-value from MaxwayCRT and code by Molei
+  # output the results
+  MaxwayCRT_result <- MaxwayCRT(
+    data = data,
+    X_on_Z_reg = X_on_Z_reg,
+    Y_on_Z_reg = Y_on_Z_reg,
+    test_hyperparams = test_hyperparams
+  )
+  p_value_MaxwayCRT <- MaxwayCRT_result |>
+    dplyr::select(value) |>
+    unname() |>
+    as.numeric()
+  
+  # run Molei's code
+  no_unlabel <- round(n*0.5)
+  set.seed(1)
+  index_unlabel <- sample(1:n, no_unlabel)
+  index_test <- setdiff(1:n, index_unlabel)
+  A_add <- X[index_unlabel]
+  Z_add <- Z[index_unlabel, ]
+  A_label <- X[index_test]
+  Z_label <- Z[index_test, ]
+  y_label <- Y[index_test]
+  MaxwayCRT <- Maxway_CRT(A_label, Z_label, y_label, A_add, Z_add, 
+                          Z_label_add = NULL, y_label_add = NULL,
+                          model_x = 'Gaussian_lasso', model_y = 'Gaussian_lasso',
+                          lambda.seq = NULL, k = NULL, seed = 1, M = 5000)
+  
+  Molei_MaxwayCRT <- MaxwayCRT$Maxway_d0CRT_pvl
+  expect_lt(
+    abs(Molei_MaxwayCRT - p_value_MaxwayCRT),
+    0.05
+  )
+})
+
+
